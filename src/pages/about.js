094a9a81007557a6350be969bcf1cccc4440254c
DakshCodes <honeysingh617522@gmail.com>
2023-04-16 14:56:29 +0530

    Debugged eslint errors

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,16 +48,16 @@ const About = () => {
                         <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 '>Biography</h2>
                             <p className='font-medium'>
-                                Hi, I'm Daksh Singh, a web developer and Designer with a passion for creating beautiful, functional,
+                                Hi, Iam Daksh Singh, a web developer and Designer with a passion for creating beautiful, functional,
                                 webapp , iam in 2nd year with multiple real world projects  in the field. I am always looking for
                                 new and innovative ways visions to life.
                             </p>
                             <p className='my-4 font-medium'>
-                                I believe that design is about more than just making things look pretty – it's about solving problems and
+                                I believe that design is about more than just making things look pretty - its about solving problems and
                                 creating intuitive, enjoyable experiences for users.
                             </p>
                             <p className='font-medium'>
-                                Whether I'm working on a website, mobile app, or
+                                Whether I am working on a website, mobile app, or
                                 other digital product, I bring my commitment to design excellence and user-centered thinking to
                                 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
                             </p>
@@ -66,7 +66,7 @@ const About = () => {
                             <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
                             <Image src={profile} alt='profile' className='w-full h-auto  rounded-2xl' />
                         </div>
- 
+
 
                         <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3' >
                             <div className='flex flex-col items-end justify-center xl:items-center'>
@@ -105,4 +105,4 @@ const About = () => {
     )
 }
 
-export default About
+export default About;
