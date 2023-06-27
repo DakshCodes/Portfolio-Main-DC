5b5098e8e41f87bcfddcdef3b2fdce9d1ce083be
DakshCodes <honeysingh617522@gmail.com>
2023-06-27 15:25:19 +0530

    End Of The day

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,49 +1,57 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-const Skill = ({name,x,y})=>{
+const Skill = ({ name, x, y }) => {
     return (
         <motion.div
-        className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
+            className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
         py-3 px-6 shadow-dark cursor-pointer absolute lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold'
 
-        whileHover={{scale:1.05}}
-        initial={{x:0,y:0}}
-        whileInView={{x:x,y:y}}
-        transition={{duration:1.5}}
+            whileHover={{ scale: 1.05 }}
+            initial={{ x: 0, y: 0 }}
+            whileInView={{ x: x, y: y }}
+            transition={{ duration: 1.5 }}
         >
-         {name}
+            {name}
         </motion.div>
     )
-} 
+}
 
 const Skills = () => {
     return (
         <>
             <h2 className='font-bold text-7xl mt-64 w-full text-center md:text-6xl md:mt-32'>Skills</h2>
-            <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]'>
-              
-               <motion.div
-               className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
-               p-8 shadow-dark cursor-pointer lg:p-6 md:p-4 xs:text-xs xs:p-2'
-
-               whileHover={{scale:1.05}}
-               >
-                Web
-               </motion.div>
-               <Skill name="HTML" x="-20vw" y="2vw" />
-               <Skill name="CSS" x="-5vw" y="-10vw" />
-               <Skill name="Javascript" x="20vw" y="6vw" />
-               <Skill name="ReactJS" x="0vw" y="12vw" />
-               <Skill name="NextJs" x="-20vw" y="-15vw" />
-               <Skill name="Framer " x="-10vw" y="-20vw" />
-               <Skill name="MySql" x="15vw" y="-12vw" />
-               <Skill name="illustrator" x="32vw" y="-5vw" />
-               <Skill name="Firebase" x="-22vw" y="14vw" />
-               <Skill name="SCSS" x="-30vw" y="5vw" />
-               <Skill name="MongoDB" x="18vw" y="18vw" />
-               <Skill name="Tailwind CSS" x="30vw" y="12vw" />
-
+            <div className='w-full h-screen relative flex items-center mt-20 justify-center rounded-full bg-circularLight lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]'>
+                <motion.div
+                    className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer lg:p-6 md:p-4 xs:text-xs xs:p-2' whileHover={{ scale: 1.05 }}>
+                    Web
+                </motion.div>
+                <Skill name="ReactJs" x="-25vw" y="-2vw" />
+                <Skill name="RestApi" x="-12vw" y="-2vw" />
+                <Skill name="CSS" x="1vw" y="-19vw" />
+                <Skill name="Javascript" x="20vw" y="6vw" />
+                <Skill name="Gsap" x="0vw" y="8vw" />
+                <Skill name="LocoMotivejs" x="0vw" y="12vw" />
+                <Skill name="Javascript" x="-16vw" y="-12vw" />
+                <Skill name="HTML" x="-9vw" y="-19vw" />
+                <Skill name="NextJs" x="10vw" y="-14vw" />
+                <Skill name="NodeJs" x="24vw" y="-2vw" />
+                <Skill name="ExpressJs" x="15vw" y="-8vw" />
+                <Skill name="Firebase" x="-22vw" y="14vw" />
+                <Skill name="SCSS" x="-30vw" y="5vw" />
+                <Skill name="MongoDB" x="-0vw" y="20vw" />
+                <Skill name="Tailwind CSS" x="21vw" y="14vw" />
+            </div>
+            <div className='ml-28 w-[60vw] mt-24  h-[80vh] relative flex items-center justify-center rounded-full bg-circularLight lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]'>
+                <motion.div
+                    className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer lg:p-6 md:p-4 xs:text-xs xs:p-2' whileHover={{ scale: 1.05 }}>
+                    Others
+                </motion.div>
+                <Skill name="C" x="-12vw" y="0vw" />
+                <Skill name="Java" x="0vw" y="-10vw" />
+                <Skill name="Ui,Ux" x="13vw" y="-13vw" />
+                <Skill name="Git,Github" x="13vw" y="-0vw" />
+                <Skill name="Python" x="0vw" y="10vw" />
             </div>
         </>
     )
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,50 +9,34 @@ import p1 from '../../public/images/projects/agency-website-cover-image.jpg'
 import Transition from '@/components/Transition'
 import { motion } from 'framer-motion'
 
-import projImg1 from "../../public/images/projects/Disney.jpg";
-import projImg2 from "../../public/images/projects/Netflix.png";
-import projImg3 from "../../public/images/projects/Hulu.jpg";
-import projImg4 from "../../public/images/projects/Imdb.jpg";
-import projImg5 from "../../public/images/projects/Tesla.png";
-import projImg6 from "../../public/images/projects/Youtube.png";
+import aniImg1 from "../../public/images/projects/a.png";
+import aniImg2 from "../../public/images/projects/a1.png";
+import aniImg3 from "../../public/images/projects/a2.png";
+import aniImg4 from "../../public/images/projects/a3.png";
+import aniImg5 from "../../public/images/projects/a4.png";
+import aniImg6 from "../../public/images/projects/a5.png";
+import aniImg7 from "../../public/images/projects/a6.png";
+import f1 from "../../public/images/projects/f1.png";
+import f2 from "../../public/images/projects/f2.png";
+import f3 from "../../public/images/projects/f3.png";
+import f4 from "../../public/images/projects/f4.png";
 
 
 const FramerImage = motion(Image)
 
-const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+const FeaturedProject = ({ title }) => {
     return (
-        <article className='w-full flex items-center justify-between
+        <article className='w-full flex items-center justify-center
         rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4'>
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark' />
-
-            <Link className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full' href={link} target="_blank">
-                <FramerImage
-                    src={img}
-                    alt={title}
-                    className='w-full h-auto'
-                    whileHover={{ scale: 1.05 }}
-                    transition={{ duration: 0.2 }}
-                />
-            </Link>
-
-            <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
-                <span className='text-primary font-medium text-2xl xs:text-base'>{type}</span>
-                <Link className='hover:underline underline-offset-2 ' href={link} target="_blank">
-                    <h2 className='my-2 w-full text-left text-4xl font-bold  sm:text-sm'>{title}</h2>
-                </Link>
-                <p className='my-2 font-medium text-dark '>{summary}</p>
-                <div className='mt-2 flex items-center '>
-                    <Link className='w-10' href={github} target="_blank"><GithubIcon /></Link>
-                    <Link className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold sm:text-base sm:px-4' href={github} target="_blank">Visit project</Link>
-                </div>
-            </div>
+            <h1 className='text-primary font-bold hover:underline cursor-pointer text-2xl lg:text-lg md:text-base'>{title}</h1>
         </article>
     )
 }
 
-const Project = ({ type, title, img, link, github }) => {
+const Project = ({ type, title, img, link, github, description }) => {
     return (
-        <article className='w-full flex-col flex items-center justify-center rounded-2xl
+        <article className='w-[35rem] flex-col flex items-center justify-center rounded-2xl
     border border-solid border-dark bg-light p-6 relative xs:p-4'>
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]' />
             <Link className='w-full cursor-pointer overflow-hidden rounded-lg ' href={link} target="_blank">
@@ -67,6 +51,7 @@ const Project = ({ type, title, img, link, github }) => {
                 <Link className='hover:underline underline-offset-2 ' href={link} target="_blank">
                     <h2 className='my-2 w-full text-left text-3xl font-bold lg:text-2xl '>{title}</h2>
                 </Link>
+                <p className='h-24 text-teal-500 font-light'>{description}</p>
                 <div className='mt-2 w-full flex items-center justify-between '>
                     <Link className='underline rounded-lg  text-lg font-semibold md:text-base' href={github} target="_blank">Visit</Link>
                     <Link className='w-8 md:w-6' href={github} target="_blank"><GithubIcon /></Link>
@@ -76,42 +61,118 @@ const Project = ({ type, title, img, link, github }) => {
     )
 }
 const projects = () => {
-    // data
-    const clones = [
+
+    const fullstack = [
 
         {
-            title: "Netflix-Clone",
-            description: "It is Amazing Website For Movies",
-            imgUrl: projImg2,
-            liveUrl: 'https://netflix-clone-dc.vercel.app/',
-            githubUrl:"https://github.com/DakshCodes/Netflix-Clone-DC"
-       
+            title: "FamilyCircle Chat App",
+            description: "User Registration and Authentication: Users can create accounts, log in, and manage their profiles. Authentication ensures secure access to the application and personalization of the user experience.",
+            imgUrl: f1,
+            liveUrl: 'https://faimly-circle-chat-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Faimly-Circle-Chat-DC"
+
         },
         {
-            title: "Hulu-Clone",
-            description: "It is Amazing Website For Movies",
-            imgUrl: projImg3,
+            title: "Blog Mad App",
+            description: "User Registration and Authentication: Users can create accounts, log in, and manage their profiles. Authentication ensures secure access to the application and personalization of the user experience.",
+            imgUrl: f2,
+            liveUrl: 'https://netflix-clone-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Blog-Mad-DC"
+        },
+        {
+            title: "To-Do App",
+            description: "To Do Application in This User Registration and Authentication: Users can create accounts, log in, and manage their profiles. And Do Crud Oprations ",
+            imgUrl: f3,
+            liveUrl: 'https://to-do-mern-dc.vercel.app/login',
+            githubUrl: "https://github.com/DakshCodes/ToDo-Mern-DC"
+
+        },
+        {
+            title: "Go Food App",
+            description: "Food App in This User Registration and Authentication: Users can create accounts, log in, and manage their profiles. ",
+            imgUrl: f4,
             liveUrl: 'https://hulu-clone-dc.vercel.app/',
-            githubUrl:"https://github.com/DakshCodes/Hulu-Clone-DC"
-          
+            githubUrl: "https://github.com/DakshCodes/Hulu-Clone-DC"
+
+        },
+        {
+            title: "Photo Gallery App",
+            description: "In This App You can Upload Your Photos In Cloud As Safe And Download AnyWhere ",
+            imgUrl: f4,
+            liveUrl: 'https://hulu-clone-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Photo-Gallery-Mern-Dc"
+
+        },
+        {
+            title: "Student Entry App",
+            description: "In This App You can Add Students And Do Crud Oprations ",
+            imgUrl: aniImg6,
+            liveUrl: 'https://hulu-clone-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Student-Entery-DC"
+
         },
 
     ];
-    const clones2 = [
+    const animation = [
+
+        {
+            title: "Modeling Page",
+            description: "A Home Page of Modeling Website Fully Animated",
+            imgUrl: aniImg7,
+            liveUrl: 'https://modeling-home-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Modeling-Home-DC"
+        },
+        {
+            title: "Shoes Page",
+            description: "A Home Page of Adidas Shoes Website Fully Animated",
+            imgUrl: aniImg1,
+            liveUrl: 'https://shoes-page-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Shoes-Page-DC"
+        },
+        {
+            title: "Cycle Page",
+            description: "A Home Page of Cycle Website Fully Animated",
+            imgUrl: aniImg6,
+            liveUrl: 'https://cycle-page-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Cycle-page-DC"
+        },
+        {
+            title: "Watch Page",
+            description: "A Home Page of Watch Website Fully Animated",
+            imgUrl: aniImg4,
+            liveUrl: 'https://watch-page-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Watch-Page-DC"
+        },
+        {
+            title: "Paper Landing Page",
+            description: "A Page of Desinged Like Paper",
+            imgUrl: aniImg3,
+            liveUrl: 'https://paper-landing-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Paper-Landing-DC"
+        },
+        {
+            title: "Lamp NavBar ",
+            description: "A Animated Lamp Navbar",
+            imgUrl: aniImg2,
+            liveUrl: 'https://lamp-nav-dc.vercel.app/',
+            githubUrl: "https://github.com/DakshCodes/Lamp-Nav-DC"
+        },
+    ];
+    const frontend = [
 
         {
             title: "Imdb-Clone",
             description: "It is Amazing Website For Movies",
-            imgUrl: projImg4,
+            imgUrl: aniImg2,
             liveUrl: 'https://imdb-clone-zeta-tan.vercel.app/',
-            githubUrl:"https://github.com/DakshCodes/Imdb-Clone-DC"
+            githubUrl: "https://github.com/DakshCodes/Imdb-Clone-DC"
         },
         {
             title: "Tesla-Clone",
             description: "It is Tesla Automobiles Componey Clone",
-            imgUrl: projImg5,
+            imgUrl: aniImg1,
             liveUrl: 'https://tesla-clone-pi-kohl.vercel.app/',
-            githubUrl:"https://github.com/DakshCodes/Tesla-Clone-DC"
+            githubUrl: "https://github.com/DakshCodes/Tesla-Clone-DC"
         },
     ];
 
@@ -128,81 +189,76 @@ const projects = () => {
                         className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'
                     />
 
-                    <div className='grid   grid-cols-12 gap-24 gap-y-24 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
-
-                        <div className='col-span-12'>
+                    <div>
+                        <div className='mt-5'>
                             <FeaturedProject
-                                github='https://github.com/DakshCodes/Disney-Clone-DC'
-                                img={projImg1}
-                                link='https://disney-clone-dc.vercel.app/'
-                                summary='It is Amazing Website For Movies '
-                                title='Disney-Clone Amazing Movies App'
-                                type='FeaturedProject'
+                                title={"Full Stack Projects"}
                             />
                         </div>
-                        <div className='col-span-6 sm:col-span-12'>
+                        <div className=' flex flex-wrap items-center justify-center gap-10 mt-5 mb-5'>
                             {
-                                clones.map((project, index) => {
-                                    return (
+                                fullstack.map((project, index) => {
+                                    return (<>
                                         <Project
-                                        key={index}
+                                            key={index}
                                             github={project.githubUrl}
                                             img={project.imgUrl}
                                             link={project.liveUrl}
                                             title={project.title}
-                                            type='Fetured Project'
+                                            description={project.description}
+                                            type='Full Stack Project'
                                         />
+                                    </>
                                     )
                                 })
                             }
 
                         </div>
-                        <div className='col-span-6 sm:col-span-12'>
+                        <div className='mt-10'>
+                            <FeaturedProject
+                                title={"Animations Projects"}
+                            />
+                        </div>
+                        <div className='flex flex-wrap items-center justify-center gap-10 mt-5 mb-5'>
                             {
-                                clones2.map((project, index) => {
+                                animation.map((project, index) => {
                                     return (
                                         <Project
-                                        key={index}
-                                            github='/'
+                                            key={index}
+                                            github={project.githubUrl}
                                             img={project.imgUrl}
                                             link={project.liveUrl}
                                             title={project.title}
-                                            type='Fetured Project'
+                                            type='Animations Project'
                                         />
                                     )
                                 })
                             }
-                        </div>
-                        <div className='col-span-12'>
-                            <FeaturedProject
-                                github='https://github.com/DakshCodes/NewTube-App-DC'
-                                img={projImg6}
-                                link='https://new-tube-app-dc.vercel.app'
-                                summary='Its A Video Streaming Platform.'
-                                title='New Tube Application'
-                                type='FeaTured Project'
-                            />
-                        </div>
-                        {/* <div className='col-span-6 sm:col-span-12'>
-                            <Project
-                                github='/'
-                                img={p1}
-                                link='/'
-                                title='/'
-                                type='/'
-                            />
-                        </div>
-                        <div className='col-span-6 sm:col-span-12'>
-                            <Project
-                                github='/'
-                                img={p1}
-                                link='/'
-                                title='/'
-                                type='/'
-                            />
-                        </div>*/}
 
-                    </div> 
+                        </div>
+                        <div className='mt-10'>
+                            <FeaturedProject
+                                title={"Frontend Projects"}
+                            />
+                        </div>
+                        <div className='flex flex-wrap items-center justify-center gap-10 mt-5 mb-5'>
+                            {
+                                frontend.map((project, index) => {
+                                    return (
+                                        <Project
+                                            key={index}
+                                            github={project.githubUrl}
+                                            img={project.imgUrl}
+                                            link={project.liveUrl}
+                                            title={project.title}
+                                            type='Frontend Project'
+                                        />
+                                    )
+                                })
+                            }
+
+                        </div>
+                    </div>
 
                 </Layout>
             </main>
